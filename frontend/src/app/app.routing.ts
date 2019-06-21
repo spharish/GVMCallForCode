342426b4228d9e7e3cbecebffd1758f558c45335
spharish <harishsparky@gmail.com>
2019-06-21 19:23:07 +0530

    Add angular frontend

---
+++ frontend/src/app/app.routing.ts
@@ -0,0 +1,19 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { RequestFormComponent } from './requestform/requestform.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'requestform', component: RequestFormComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
